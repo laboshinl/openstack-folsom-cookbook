52a3000944ea93b7985fd840b7f5619772b42b09
Leonid Laboshin <laboshinl@gmail.com>
2012-09-27 21:32:40 +0400

    Update cloud-common/attributes/default.rb

--- cloud-common/attributes/default.rb
+++ cloud-common/attributes/default.rb
@@ -3,7 +3,9 @@ set[:mysql][:password]="my_super_secret"
 set[:controller][:vg_name]="my_vg_name"
 
 set[:controller][:private_ip]="10.10.10.10"
+set[:controller][:private_interface]="eth1"
 set[:controller][:public_ip]="8.8.8.8"
+set[:controller][:public_interface]="eth0"
 set[:controller][:private_netmask]="8"
 
 set[:keystone][:token]="my_super_secret"
